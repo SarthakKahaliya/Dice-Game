59a8b0be1e3344b21c77c19c8292f08ead4e1ab5
SarthakKahaliya <sarthakkahaliya@gmail.com>
2022-05-24 21:20:30 -0400

    Project Upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = randomNumber();
+var randomNumber2 = randomNumber();
+
+function randomNumber() {
+    return Math.floor(Math.random()*6)+1;
+}
+
+
+document.querySelector(".img1").setAttribute('src', 'images/dice'+randomNumber1+'.png');
+document.querySelector(".img2").setAttribute('src', 'images/dice'+randomNumber2+'.png');
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+}else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+}else{
+    document.querySelector("h1").innerHTML = "Its a Draw";
+}
+
+console.log(randomNumber1, randomNumber2);
