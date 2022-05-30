87dc9128e67011f792d2f93703f686025fb516fd
SarthakKahaliya <sarthakkahaliya@gmail.com>
2022-05-30 18:39:00 -0400

    Changed the Game

--- index.js
+++ index.js
@@ -1,20 +1,81 @@
-var randomNumber1 = randomNumber();
-var randomNumber2 = randomNumber();
-
 function randomNumber() {
-    return Math.floor(Math.random()*6)+1;
+  return Math.floor(Math.random() * 6) + 1;
 }
 
+var p1 = 0;
+var p2 = 0;
 
-document.querySelector(".img1").setAttribute('src', 'images/dice'+randomNumber1+'.png');
-document.querySelector(".img2").setAttribute('src', 'images/dice'+randomNumber2+'.png');
+var complete = 0;
 
-if(randomNumber1>randomNumber2){
+document.querySelector(".btn").addEventListener("click", run);
+
+function run() {
+  var randomNumber1 = randomNumber();
+  var randomNumber2 = randomNumber();
+
+
+  if(complete == 0){
+  p1 += randomNumber1;
+  p2 += randomNumber2;
+  }else{
+    p1 = 0;
+    p2 = 0;
+    complete = 0
+    document.querySelector("h1").innerHTML = "🏁 Race!! 🏁";
+  }
+
+
+  if ((p1 > 101) & (p2 > 101)) {
+    document.querySelector("h1").innerHTML = "Race is Draw";
+    p1 = 101;
+    p2 = 101;
+    document
+      .querySelector(".p1")
+      .setAttribute("style", "margin-left: " + p1 + "%");
+    document
+      .querySelector(".p2")
+      .setAttribute("style", "margin-left: " + p2 + "%");
+    complete = 1;
+  } else if (p1 > 101) {
     document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
-}else if(randomNumber1<randomNumber2){
+    p1 = 101;
+    document
+      .querySelector(".p1")
+      .setAttribute("style", "margin-left: " + p1 + "%");
+    document
+      .querySelector(".p2")
+      .setAttribute("style", "margin-left: " + p2 + "%");
+    complete = 1;
+  } else if (p2 > 101) {
     document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
-}else{
-    document.querySelector("h1").innerHTML = "Its a Draw";
-}
+    p2 = 101;
+    document
+      .querySelector(".p1")
+      .setAttribute("style", "margin-left: " + p1 + "%");
+    document
+      .querySelector(".p2")
+      .setAttribute("style", "margin-left: " + p2 + "%");
+    complete = 1;
+  }
 
-console.log(randomNumber1, randomNumber2);
+  if (complete == 0) {
+    document
+      .querySelector(".p1")
+      .setAttribute("style", "margin-left: " + p1 + "%");
+    document
+      .querySelector(".p2")
+      .setAttribute("style", "margin-left: " + p2 + "%");
+    document
+      .querySelector(".img1")
+      .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document
+      .querySelector(".img2")
+      .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    document.querySelector(".btn").textContent = "Roll Again";
+
+    console.log(randomNumber1, randomNumber2);
+  }
+
+  
+}
